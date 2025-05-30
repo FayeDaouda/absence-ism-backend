5710fac01ff37d5df473b4dad53c7d2a65db6272
Daouda FAYE <daoudafaye2017@gmail.com>
2025-05-30 22:23:38 +0000

    Ajout du PointageController avec endpoint /ping

--- src/main/java/com/ism/absences/entity/User.java
+++ src/main/java/com/ism/absences/entity/User.java
@@ -8,9 +8,42 @@ import java.util.Set;
 public class User {
     @Id
     private String id;
-    private String username; // ou email
+    private String email;        // Remplace username par email
     private String password;
-    private Set<String> roles; // ex: "ROLE_ADMIN", "ROLE_VIGILE", "ROLE_ETUDIANT"
+    private Set<String> roles;   // ex: "ROLE_ADMIN", "ROLE_VIGILE", "ROLE_ETUDIANT"
 
-    // getters et setters
+    // Getters et Setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
 }
+
