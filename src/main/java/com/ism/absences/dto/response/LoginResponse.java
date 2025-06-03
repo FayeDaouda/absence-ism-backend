f253ee7120ff9c3eae097a032d89fa2e2228c462
Daouda FAYE <daoudafaye2017@gmail.com>
2025-06-03 04:03:48 +0000

    token44

--- src/main/java/com/ism/absences/dto/response/LoginResponse.java
+++ src/main/java/com/ism/absences/dto/response/LoginResponse.java
@@ -13,5 +13,29 @@ public class LoginResponse {
         this.token = token;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
     // Getters & setters
 }
--- src/main/java/com/ism/absences/service/AuthService.java
+++ src/main/java/com/ism/absences/service/AuthService.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import com.ism.absences.dto.request.LoginRequest;
 import com.ism.absences.dto.response.LoginResponse;
 import com.ism.absences.entity.Utilisateur;
