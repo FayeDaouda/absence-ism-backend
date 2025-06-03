66bb2fbb66f7472807b40056d40f576e13c019f4
Daouda FAYE <daoudafaye2017@gmail.com>
2025-06-03 00:48:12 +0000

    se

--- src/main/java/com/ism/absences/security/SecurityConfig.java
+++ src/main/java/com/ism/absences/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.ism.absences.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,5 +19,9 @@ public class SecurityConfig {
             .formLogin(form -> form.disable());    // désactive la connexion formulaire
 
         return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
