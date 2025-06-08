b125b0702d7ec3413c40155ca91c987258bd4a0a
Daouda FAYE <daoudafaye2017@gmail.com>
2025-06-08 16:32:32 +0000

    repo22

--- absences/src/main/java/com/ism/absences/controller/JustificationController.java
+++ absences/src/main/java/com/ism/absences/controller/JustificationController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.ism.absences.util.MultipartInputStreamFileResource;
+import org.springframework.web.client.RestTemplate;
+
 
 
 import java.util.List;
